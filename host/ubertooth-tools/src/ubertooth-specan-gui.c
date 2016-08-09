cd31068ac05e08194ea2c72e190f1b2fda055a95
Hannes Ellinger <hannes.ellinger@eas.iis.fraunhofer.de>
2016-08-09 10:06:16 +0200

    implement an SDL based spectrum analyzer similar to the python tool ubertooth-specan-ui which is much faster than the original

---
+++ host/ubertooth-tools/src/ubertooth-specan-gui.c
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2016 Hannes Ellinger
+ *
+ * This file is part of Project Ubertooth.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include <getopt.h>
+#include "ubertooth.h"
+#include <SDL2/SDL.h>
+#include <unistd.h>
+
+#define DEPTH 175
+
+// The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+// The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+int w, h;
+int low_dbm = -120;
+int high_dbm = 0;
+int low_freq = 2400;
+int high_freq = 2500;
+
+int8_t rssi_values[DEPTH][600];
+int rssi_index = 0;
+
+int window_init()
+{
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		return -1;
+	}
+
+	//Create window
+	gWindow = SDL_CreateWindow( "Ubertooth spectrum analyzer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN );
+	if( gWindow == NULL )
+	{
+		printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+		return -1;
+	}
+
+	//Create renderer for window
+	gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+	if( gRenderer == NULL )
+	{
+		printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+		return -1;
+	}
+
+	//Initialize renderer color
+	SDL_SetRenderDrawBlendMode(gRenderer, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 255 );
+	SDL_RenderClear(gRenderer);
+
+	return 0;
+}
+
+void window_close()
+{
+	//Destroy window
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gRenderer = NULL;
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int dbm_to_y(int dbm)
+{
+	return (high_dbm - dbm) * h / (high_dbm - low_dbm);
+}
+
+int mhz_to_x(int freq)
+{
+	return (freq - low_freq) * w / (high_freq - low_freq);
+}
+
+void drawReticle()
+{
+	SDL_Rect area;
+
+	area.x=0;
+	area.y=0;
+	SDL_GetWindowSize(gWindow, &w, &h);
+	area.w=w;
+	area.h=h;
+
+	//Clear screen
+	SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 10 );
+	SDL_RenderFillRect(gRenderer, &area);
+
+	for (int i=low_dbm; i<=high_dbm; i+=10)
+	{
+		SDL_SetRenderDrawColor( gRenderer, 0, 0, 127, 255 );
+		SDL_RenderDrawLine( gRenderer, 0, dbm_to_y(i), w, dbm_to_y(i));
+	}
+	for (int i=low_freq; i<=high_freq; i+=10)
+	{
+		SDL_SetRenderDrawColor( gRenderer, 0, 0, 127, 255 );
+		SDL_RenderDrawLine( gRenderer, mhz_to_x(i), 0, mhz_to_x(i), h);
+	}
+}
+
+void drawLine()
+{
+	int8_t* max = (int8_t*)malloc(high_freq-low_freq+1 * sizeof(int8_t));
+	memset(max, -128, high_freq-low_freq+1);
+
+	for (int i=0; i<high_freq-low_freq+1; i++) {
+		for (int j=0; j<DEPTH; j++) {
+			if (max[i] < rssi_values[j][i])
+				max[i] = rssi_values[j][i];
+		}
+	}
+
+	for (int i=0; i<high_freq-low_freq; i++)
+	{
+		SDL_SetRenderDrawColor( gRenderer, 255, 255, 255, 255 );
+		SDL_RenderDrawLine( gRenderer, mhz_to_x(i+low_freq), dbm_to_y(rssi_values[rssi_index][i]), mhz_to_x(i+low_freq+1), dbm_to_y(rssi_values[rssi_index][i+1]));
+		SDL_SetRenderDrawColor( gRenderer, 0, 255, 0, 255 );
+		SDL_RenderDrawLine( gRenderer, mhz_to_x(i+low_freq), dbm_to_y(max[i]), mhz_to_x(i+low_freq+1), dbm_to_y(max[i+1]));
+	}
+}
+
+void cb_specan_gui(ubertooth_t* ut __attribute__((unused)), void* args __attribute__((unused)))
+{
+	usb_pkt_rx rx = fifo_pop(ut->fifo);
+
+	/* process each received block */
+	for (int i = 0; i < DMA_SIZE-2; i += 3)
+	{
+		uint16_t frequency = (rx.data[i] << 8) | rx.data[i + 1];
+		int8_t rssi = (int8_t)rx.data[i + 2];
+
+		rssi_values[rssi_index][frequency-low_freq] = rssi - 54;
+
+		// end of a frame
+		if (frequency == high_freq)
+		{
+			drawReticle();
+
+			drawLine();
+
+			//Update screen
+			SDL_RenderPresent( gRenderer );
+
+			rssi_index = (rssi_index+1) % DEPTH;
+		}
+	}
+}
+
+static void usage(FILE *file)
+{
+	fprintf(file, "ubertooth-specan - output a continuous stream of signal strengths\n");
+	fprintf(file, "Usage:\n");
+	fprintf(file, "\t-h this help\n");
+	fprintf(file, "\t-l lower frequency (default 2400)\n");
+	fprintf(file, "\t-u upper frequency (default 2500)\n");
+	fprintf(file, "\t-U<0-7> set ubertooth device to use\n");
+}
+
+static void handle_events(ubertooth_t* ut)
+{
+	// SDL event handler
+	SDL_Event e;
+
+	// Handle events on queue
+	while( SDL_PollEvent( &e ) != 0 )
+	{
+		// User requests quit
+		if( e.type == SDL_QUIT )
+		{
+			ut->stop_ubertooth = 1;
+		}
+
+		// Window resized
+		if( e.type == SDL_WINDOWEVENT_SIZE_CHANGED || e.type == SDL_WINDOWEVENT_RESIZED )
+		{
+			SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 255 );
+			SDL_RenderClear(gRenderer);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int opt, r = 0;
+	char ubertooth_device = -1;
+
+	ubertooth_t* ut = NULL;
+
+	memset(rssi_values, -128, sizeof(rssi_values));
+
+	while ((opt=getopt(argc,argv,"h:l::u::U:")) != EOF) {
+		switch(opt) {
+		case 'l':
+			if (optarg)
+				low_freq= atoi(optarg);
+			else
+				printf("lower: %d\n", low_freq);
+			break;
+		case 'u':
+			if (optarg)
+				high_freq= atoi(optarg);
+			else
+				printf("upper: %d\n", high_freq);
+			break;
+		case 'U':
+			ubertooth_device = atoi(optarg);
+			break;
+		case 'h':
+			usage(stdout);
+			return 0;
+		default:
+			usage(stderr);
+			return 1;
+		}
+	}
+
+	ut = ubertooth_start(ubertooth_device);
+
+	if (ut == NULL) {
+		usage(stderr);
+		return 1;
+	}
+
+	r = ubertooth_check_api(ut);
+	if (r < 0)
+		return 1;
+
+	/* Clean up on exit. */
+	register_cleanup_handler(ut, 0);
+
+	//Start up SDL and create window
+	r = window_init();
+	if (r < 0)
+		return r;
+
+	// init USB transfer
+	r = ubertooth_bulk_init(ut);
+	if (r < 0)
+		return r;
+
+	r = ubertooth_bulk_thread_start();
+	if (r < 0)
+		return r;
+
+	// tell ubertooth to start specan and send packets
+	r = cmd_specan(ut->devh, low_freq, high_freq);
+	if (r < 0)
+		return r;
+
+	// receive and process each packet
+	while(!ut->stop_ubertooth) {
+		ubertooth_bulk_receive(ut, cb_specan_gui, NULL);
+		handle_events(ut);
+	}
+
+	window_close();
+
+	ubertooth_bulk_thread_stop();
+
+	ubertooth_stop(ut);
+	fprintf(stderr, "Ubertooth stopped\n");
+
+	return 0;
+}
